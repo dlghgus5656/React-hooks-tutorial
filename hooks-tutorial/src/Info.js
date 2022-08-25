3a93e3fbb7e43417563dea510087eb43bb377cd5
Lee ho hyeon <82634597+dlghgus5656@users.noreply.github.com>
2022-08-25 17:00:36 +0900

    뒷정리 하기(cleanup)
    useEffect 는 기본적으로 렌더링 되고난 직후마다 실행되며, 두번째 파라미터 배열에 무엇을 넣느냐에 따라 실행되는 조건이 달라집니다.
    
    만약 컴포넌트가 언마운트되기 전이나, 업데이트 되기 직전에 어떠한 작업을 수행하고 싶다면 useEffect 에서 뒷정리(cleanup) 함수를 반환해주어야 합니다.
    
    만약에, 오직 언마운트 될 때만 뒷정리 함수를 호출하고 싶으시다면 useEffect 함수의 두번째 파라미터에 비어있는 배열을 넣으시면 됩니다.

--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -5,7 +5,16 @@ const Info = () => {
   const [nickname, setNickname] = useState("");
 
   useEffect(() => {
-    console.log("렌더링이 완료되었습니다! 이번엔 마운트 될 때만 실행됩니다.");
+    // console.log(
+    //   "렌더링이 완료되었습니다! 이번엔 마운트 될 때만 실행되고 특정 값이 업데이트 될 때 실행합니다."
+    // );
+    console.log("effect");
+    console.log(name);
+
+    return () => {
+      console.log("cleanup");
+      console.log(name);
+    };
     // console.log(
     //   {
     //     name,
@@ -15,7 +24,10 @@ const Info = () => {
     // 만약 useEffect 에서 설정한 함수가 컴포넌트가 화면에 가장 처음 렌더링 될 때만 실행되고
     // 업데이트 할 경우에는 실행 할 필요가 없는 경우엔 함수의 두번째 파라미터로 비어있는 배열을 넣어주시면 된다.
     // );
-  }, []);
+    //    특정 값이 업데이트 될 때만 실행하고 싶을 때
+    //    useEffect 를 사용 할 때 특정 값이 변경이 될 때만 호출하게 하고 싶을 경우도 있을 때
+    //    바로, useEffect 의 두번째 파라미터로 전달되는 배열 안에 검사하고 싶은 값을 넣어주시면 된다.
+  }, [name]);
 
   const onChangeName = (e) => {
     setName(e.target.value);
