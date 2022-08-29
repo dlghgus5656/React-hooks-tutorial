4a386d0dfbe69d6fdbfa24ce09d6a7843da73fd9
Lee ho hyeon <82634597+dlghgus5656@users.noreply.github.com>
2022-08-29 17:33:41 +0900

    useReducer
    useReducer 는 useState 보다 컴포넌트에서 더 다양한 상황에 따라 다양한 상태를 다른 값으로 업데이트해주고 싶을 때 사용하는 Hook 입니다
    
    인풋 상태 관리

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,24 +1,27 @@
 import React from "react";
 import { useState } from "react";
-// import Counter from "./Counter";
+import Counter from "./Counter";
 import Info from "./Info";
+import ContextSample from "./ContextSample";
 
 const App = () => {
   // return <Counter />;
-  const [visible, setVisible] = useState(false);
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? "숨기기" : "보이기"}
-      </button>
-      <hr />
-      {visible && <Info />}
-    </div>
-  );
+  return <Info />;
+  // const [visible, setVisible] = useState(false);
+  // return (
+  //   <div>
+  //     <button
+  //       onClick={() => {
+  //         setVisible(!visible);
+  //       }}
+  //     >
+  //       {visible ? "숨기기" : "보이기"}
+  //     </button>
+  //     <hr />
+  //     {visible && <Info />}
+  //   </div>
+  // );
+  // return <ContextSample />;
 };
 
 export default App;
--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -1,53 +1,68 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useReducer } from "react";
+
+function reducer(state, action) {
+  return {
+    ...state,
+    [action.name]: action.value,
+  };
+}
 
 const Info = () => {
-  const [name, setName] = useState("");
-  const [nickname, setNickname] = useState("");
-
-  useEffect(() => {
-    // console.log(
-    //   "렌더링이 완료되었습니다! 이번엔 마운트 될 때만 실행되고 특정 값이 업데이트 될 때 실행합니다."
-    // );
-    console.log("effect");
-    console.log(name);
-
-    return () => {
-      console.log("cleanup");
-      console.log(name);
-    };
-    // console.log(
-    //   {
-    //     name,
-    //     nickname,
-    //   }
-    //    ! 마운트 될 때만 실행하고 싶을 때
-    // 만약 useEffect 에서 설정한 함수가 컴포넌트가 화면에 가장 처음 렌더링 될 때만 실행되고
-    // 업데이트 할 경우에는 실행 할 필요가 없는 경우엔 함수의 두번째 파라미터로 비어있는 배열을 넣어주시면 된다.
-    // );
-    //    특정 값이 업데이트 될 때만 실행하고 싶을 때
-    //    useEffect 를 사용 할 때 특정 값이 변경이 될 때만 호출하게 하고 싶을 경우도 있을 때
-    //    바로, useEffect 의 두번째 파라미터로 전달되는 배열 안에 검사하고 싶은 값을 넣어주시면 된다.
-  }, [name]);
-
-  const onChangeName = (e) => {
-    setName(e.target.value);
+  // const [name, setName] = useState("");
+  // const [nickname, setNickname] = useState("");
+  const [state, dispatch] = useReducer(reducer, {
+    name: "",
+    nickname: "",
+  });
+  const { name, nickname } = state;
+  const onChange = (e) => {
+    dispatch(e.target);
   };
-  const onChangeNickname = (e) => {
-    setNickname(e.target.value);
-  };
+
+  // useEffect(() => {
+  // console.log(
+  //   "렌더링이 완료되었습니다! 이번엔 마운트 될 때만 실행되고 특정 값이 업데이트 될 때 실행합니다."
+  // );
+  // console.log("effect");
+  // console.log(name);
+
+  // return () => {
+  //   console.log("cleanup");
+  //   console.log(name);
+  // };
+  // console.log(
+  //   {
+  //     name,
+  //     nickname,
+  //   }
+  //    ! 마운트 될 때만 실행하고 싶을 때
+  // 만약 useEffect 에서 설정한 함수가 컴포넌트가 화면에 가장 처음 렌더링 될 때만 실행되고
+  // 업데이트 할 경우에는 실행 할 필요가 없는 경우엔 함수의 두번째 파라미터로 비어있는 배열을 넣어주시면 된다.
+  // );
+  //    특정 값이 업데이트 될 때만 실행하고 싶을 때
+  //    useEffect 를 사용 할 때 특정 값이 변경이 될 때만 호출하게 하고 싶을 경우도 있을 때
+  //    바로, useEffect 의 두번째 파라미터로 전달되는 배열 안에 검사하고 싶은 값을 넣어주시면 된다.
+  // }, []);
+
+  // const onChangeName = (e) => {
+  //   setName(e.target.value);
+  // };
+  // const onChangeNickname = (e) => {
+  //   setNickname(e.target.value);
+  // };
 
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} />
-        <input value={nickname} onChange={onChangeNickname} />
+        <input name="name" value={name} onChange={onChange} />
+        <input nickname="nickname" value={nickname} onChange={onChange} />
       </div>
       <div>
         <div>
           <b>이름:</b> {name}
         </div>
         <div>
-          <b>닉네임:</b> {nickname}
+          <b>닉네임: </b> {nickname}
         </div>
       </div>
     </div>
